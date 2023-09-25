d9a4071492c87c5106b8fa37c5eafb845654c51e
AbelHristodor <habelor@gmail.com>
2023-09-25 17:05:18 +0200

    base nvim conf

---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,14 @@
+function ColorMyPencils(color) 
+	color = color or "nord"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+vim.g.nord_disable_background = true
+
+require('nord').set()
+
+ColorMyPencils()
